892e1e810584a330ac79cba07189e8357ee50b57
koolguru <koolguru2@gmail.com>
2014-04-14 08:03:46 -0700

    Functional. Fixed XNA errors

---
+++ HuntTheWumpusXNAGame/HuntTheWumpusXNAGame/HuntTheWumpusXNAGame/HighScore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HuntTheWumpusXNAGame
+{
+    class HighScore
+    {
+        public int NumberOfTurns { get; set; }
+        public int NumberOfArrows { get; set; }
+        public int GoldLeft { get; set; }
+
+        public HighScore(int turns, int arrows, int gold)
+        {
+            NumberOfTurns = turns;
+            NumberOfArrows = arrows;
+            GoldLeft = gold;
+        }
+        public List<int> TopTenScores(int totalScore)
+        {
+            List<int> highScoreList = new List<int>();
+            //pulls an existing high score list
+            //gets totalScore from ScoreCalculations
+            //compares totalScore with entire list
+            //places it in the list if it belongs in the top ten and makes a new list 
+            ScoreCalculations(100, 200, 300);
+            return highScoreList; //final list with new score compared with stored scores
+        }
+        public int ScoreCalculations(int numberOfTurns, int goldLeft, int numberOfArrowsLeft)
+        {
+            int totalScore = 100;
+            totalScore = 100 - numberOfTurns + goldLeft + (10 * numberOfArrowsLeft);
+            return totalScore; //total score
+        }
+        public void ScoreHandler()
+        {
+            //keep track of number of turns
+            int numberOfTurns = 1;
+            //keep track of gold left
+            int goldLeft = 1;
+            //keep track of arrows
+            int arrowsLeft = 1;
+            //keep updating during game
+            //Call the ScoreCalculator() and provide it with the above variables as paramters 
+        }
+    }
+}
